93572345e444ba29ea609af0a006961aa762734b
nenis11andres <nenis11andres@gmail.com>
2025-10-28 18:29:36 +0100

    Modificamos archivos para lanzamiento de instancias

--- terraform/scripts/apache.sh
+++ terraform/scripts/apache.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -eux
 
-# Actualizar e instalar docker
+# Actualizar e instalar Docker y dependencias
 apt-get update -y
 apt-get install -y docker.io docker-compose git
 systemctl start docker
 systemctl enable docker
 
-# Asegurar que el usuario admin puede usar docker
+# Usuario admin puede usar Docker
 usermod -aG docker admin
 
 # Clonar repositorio
 cd /home/admin
 git clone https://github.com/nenis11andres/RA2-ec2.git
 
-# Arrancar contenedor
+# Cambiar permisos de la carpeta gym para Apache
+chown -R admin:admin RA2-ec2/gym
+
+# Levantar contenedor Apache
 cd RA2-ec2/docker
 docker-compose up -d apache
--- terraform/scripts/nginx.sh
+++ terraform/scripts/nginx.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 set -eux
 
-# Actualizar e instalar docker
+# Actualizar e instalar Docker y dependencias
 yum update -y
 yum install -y docker git
 systemctl start docker
 systemctl enable docker
 
-# Instalar docker compose
+# Instalar Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Asegurar que el usuario ec2-user puede usar docker
+# Usuario ec2-user puede usar Docker
 usermod -aG docker ec2-user
 
 # Clonar repositorio
 cd /home/ec2-user
 git clone https://github.com/nenis11andres/RA2-ec2.git
 
-# Arrancar contenedor
+# Cambiar permisos de la carpeta gym para PHP-FPM
+chown -R ec2-user:ec2-user RA2-ec2/gym
+
+# Levantar contenedor Nginx
 cd RA2-ec2/docker
 docker-compose up -d nginx
